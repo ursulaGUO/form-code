fce491b33f4c2a6bef8437bd0601c3dfedbfcf6f
bla G <blag@bladeMacBook-Pro.local>
2021-05-17 23:38:29 +0800

    set background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,49 @@
-function setup() {
-  createCanvas(innerWidth, innerHeight)
-  background(0)
-  let x = width / 2
-  let y = height / 2
-  let r = width / 4
-  ellipse(x, y, r)
+const blue_sky = {
+    r: 93, 
+    g: 229, 
+    b: 250
+};
+let xs = [];
+let ys = [];
+let snows = 40;
+let d = 10;
+let cat1;
+
+function get_random(min, max) {
+	return Math.random() * (max - min) + min;
 }
+
+
+  
+function setup() {
+	createCanvas(innerWidth, innerHeight);
+	background(blue_sky.r, blue_sky.g, blue_sky.b);
+	
+	for (i = 0; i < snows; i += 1) {
+		xs[i] = get_random(0, width);
+		ys[i] = get_random(0, height);
+	}
+
+	cat1 = loadImage('images/cat1.png');
+
+}
+
+function draw() {
+
+	/* snow*/
+	background(blue_sky.r, blue_sky.g, blue_sky.b);
+	for (i = 0; i < snows; i += 1) {
+		if (ys[i] > height) {
+			ys[i] = 0;
+		} else {
+			ys[i] += 3;
+		}
+		strokeWeight(0);
+		ellipse(xs[i],ys[i],d);
+
+	}
+	fill(255,255,255);
+
+	image(cat1, width / 5 * 4, height / 5 * 4);
+}
+
